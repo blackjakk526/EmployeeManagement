fe406602d21fdb6d3027a18119d62c156ca92ac6
Alan Gounley <agounley@gmail.com>
2019-11-17 21:26:51 -0500

    Added json file

--- EmployeeApp/App.xaml.cs
+++ EmployeeApp/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,6 +17,18 @@ namespace EmployeeApp
     {
         public List<Employee> employeeList = new List<Employee>();
 
+        public App()
+        {
+            LoadJson();
+        }
 
+        void LoadJson()
+        {
+            using(StreamReader r = new StreamReader("C:/Users/agoun/source/repos/EmployeeManagement/EmployeeApp/json1.json"))
+            {
+                string json = r.ReadToEnd();
+                Employee[] employeeArray = JsonConvert.DeserializeObject <Employee[]>(json);
+            }
+        }
     }
 }
